9c3f27a6edffc077f72ffe9d4bbe479d6eda9f6e
Hi, there! <arthurwanggang@yeah.net>
2023-03-25 18:18:35 +0800

    feat: refactor the structure

--- code/frontend/dashboard-app/src/component/ContentHeader/Index.tsx
+++ code/frontend/dashboard-app/src/component/ContentHeader/Index.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
 import "./Index.css";
+import { Button, IconButton } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import SettingsIcon from '@mui/icons-material/Settings';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import SearchIcon from '@mui/icons-material/Search';
 
 
 function ContentHeader() {
 
     return (
         <div className="ContentHeaderContainer">
-            134t
+            <Button variant="outlined" className="search-button-wrapper" startIcon={<SearchIcon />}>
+                How can I assist you today?
+            </Button>
+            <IconButton className="icon-button-outline">
+                <GitHubIcon />
+            </IconButton>
+            <IconButton className="icon-button-outline">
+                <NotificationsNoneOutlinedIcon />
+            </IconButton>
+            <IconButton className="icon-button-outline">
+                <SettingsIcon />
+            </IconButton>
         </div>
     );
 }
--- code/frontend/dashboard-app/src/component/Menu/Index.tsx
+++ code/frontend/dashboard-app/src/component/Menu/Index.tsx
@@ -1,11 +1,44 @@
 import * as React from "react";
 import "./Index.css"
-import Paper from '@mui/material/Paper';
+import { Divider, ListItemIcon, ListItemText, MenuItem, MenuList } from "@mui/material";
+import { ContentCopy, ContentPaste} from "@mui/icons-material";
+import ArticleIcon from '@mui/icons-material/Article';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 export  default function MenuSet() {
     return (
-        <Paper className="PaperMenuContainer">
-            header content
-        </Paper>
+        <div className="paperMenuContainer">
+
+            <div className="title-wrapper">
+                MACHINE LEARNING
+            </div>
+            <Divider light />
+            <div className="menu-wrapper">
+
+                <MenuList>
+                    <MenuItem>
+                        <ListItemIcon>
+                            <ArticleIcon style={{color: "#007FFF"}} fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText >Overview</ListItemText>
+                        <KeyboardArrowRightIcon style={{color: "#007FFF"}} fontSize="small" />
+                    </MenuItem>
+                    <MenuItem>
+                        <ListItemIcon>
+                            <ContentCopy style={{color: "#007FFF"}} fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Machine Learning</ListItemText>
+                        <KeyboardArrowRightIcon style={{color: "#007FFF"}} fontSize="small" />
+                    </MenuItem>
+                    <MenuItem>
+                        <ListItemIcon>
+                            <ContentPaste style={{color: "#007FFF"}} fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Feature</ListItemText>
+                        <KeyboardArrowRightIcon style={{color: "#007FFF"}} fontSize="small" />
+                    </MenuItem>
+                </MenuList>
+            </div>
+        </div>
     )
 }
